4db51a574833bd965762be9d99c82438db640ca9
Sean Leffler <sean@errno.com>
2016-02-03 16:16:55 -0800

    Added lazy instantiation macro for use items.

--- src/macros.rs
+++ src/macros.rs
@@ -22,3 +22,53 @@ macro_rules! is_eq {
         is_eq_test!($($thingA)*, $($thingB)*);
     );
 }
+
+macro_rules! lazy_single_instantiate {
+    (@remove
+        ($($remove:tt)*) $name:ident
+        { $($searched:tt)* }
+        { ($($input:tt)*) => { $($body:tt)* } $($unsearched:tt)* }
+    ) => (
+        is_eq! {
+            if ($($remove)*) == ($($input)*) {
+                lazy_single_instantiate! {
+                    $name:
+                    $($searched)*
+                    ($($input)*) => {}
+                    $($unsearched)*
+                }
+            } else {
+                lazy_single_instantiate! {
+                    @remove
+                    ($($remove)*)
+                    $name
+                    { $($searched)* ($($input)*) => { $($body)* } }
+                    { $($unsearched)* }
+                }
+            }
+        }
+    );
+    (@recurse $all:tt { $($build:tt)* } $name:ident ( $($input:tt)* ) => { $($body:tt)* } $($more:tt)*) => (
+        lazy_single_instantiate! {
+            @recurse
+            $all
+            {
+                $($build)*
+                ($($input)*) => (
+                    $($body)*
+                    lazy_single_instantiate!(@remove ($($input)*) $name {} $all);
+                );
+            }
+            $name
+            $($more)*
+        }
+    );
+    (@recurse $all:tt { $($build:tt)* } $name:ident) => (
+        macro_rules! $name {
+            $($build)*
+        }
+    );
+    ($name:ident: $($more:tt)*) => (
+        lazy_single_instantiate!(@recurse { $($more)* } {} $name $($more)*);
+    );
+}
